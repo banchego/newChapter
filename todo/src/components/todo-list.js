7b3e72fb4ccd6cdb6768cf8f287bee3d60dd223b
banchego <nbnikitanb@gmail.com>
2022-08-25 16:48:03 -0600

    Arrays as props

--- todo/src/components/todo-list.js
+++ todo/src/components/todo-list.js
@@ -2,15 +2,23 @@ import React from 'react';
 import TodoListItem from './todo-list-item';
 
 
-  const TodoList = () => {
+  const TodoList = ({todos}) => {
+
+    const elements = todos.map((item)=> {
+
     return (
-      <ul>
+      <li>
+      <TodoListItem {...item} /> 
+      </li>
+    );
+    });
+
+      return (<ul>
         
-        <li><TodoListItem label='Drink Coffee'/> </li>
-        <li><TodoListItem label='Build React App' important /> </li>
-        
+       {elements}
       </ul>
     );
-  };
+      };
+  
 
   export default TodoList;
--- todo/src/index.js
+++ todo/src/index.js
@@ -6,13 +6,20 @@ import TodoList from './components/todo-list';
 
 
 const App = () => {
+
+  const todoData= [
+    {label: 'Drink Coffee', important: false},
+    {label: 'Make Awesome App', important: true},
+    {label: 'Have a lunch', important: false},
+  ];
+
     return (
     <div>
     <AppHeader />
     <SearchPanel />
-    <TodoList />
+    <TodoList todos={todoData}/>
   </div>
-    );
+  );
 };
 
 
