629b0d2d3a73e39e95f18ee96faa9cdac4632e25
banchego <nbnikitanb@gmail.com>
2022-08-27 19:26:42 -0600

    Components-classes

--- todo/src/components/item-status-filter/item-status-filter.js
+++ todo/src/components/item-status-filter/item-status-filter.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
-  return (
+
+export default class ItemStatusFilter extends Component {
+  render() {
+    return (
     <div className="btn-group">
       <button type="button"
               className="btn btn-info">All</button>
@@ -12,7 +14,6 @@ const ItemStatusFilter = () => {
       <button type="button"
               className="btn btn-outline-secondary">Done</button>
     </div>
-  );
+    );
+}
 };
-
-export default ItemStatusFilter;
--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './todo-list-item';
+export default class TodoListItem extends Component {
 
-const TodoListItem = ({ label, important = false }) => {
-
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
-
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
+  render() {
+    const { label, important = false } = this.props;
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+  
+    return (
+      <span className="todo-list-item">
+        <span
+          className="todo-list-item-label"
+          style={style}>
+          {label}
+        </span>
+  
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right">
+          <i className="fa-solid fa-triangle-exclamation"></i>
+        </button>
+  
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa-solid fa-trash-can"/>
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa-solid fa-triangle-exclamation"></i>
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa-solid fa-trash-can"/>
-      </button>
-    </span>
-  );
+    );
+  }
 };
 
-export default TodoListItem;
+
